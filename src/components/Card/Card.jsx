48c519fe848555c1b6fea33fb5a185559e2351b7
Tom Gillard <tomg@seek.com.au>
2022-02-18 09:09:09 +1100

    Fix image set

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -5,7 +5,7 @@ import './Card.css';
 export const Card = ({ monster }) => (
   <div className="card-container">
     <img
-      src={`https://robohash.org/${monster.id}?set=2&size=180x180`}
+      src={`https://robohash.org/${monster.id}?set=set2&size=180x180`}
       alt="monster avatar"
     />
     <h2>{monster.name}</h2>
